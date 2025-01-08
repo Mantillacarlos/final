dadce4431d1d99722e5f3a33e6bf7005f49908e2
Mantillacarlos <136761621+Mantillacarlos@users.noreply.github.com>
2025-01-07 21:39:27 -0500

    sonido

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-document.getElementById("change-bg").addEventListener("click", () => {
-    document.body.style.backgroundColor = 
-        document.body.style.backgroundColor === "lightblue" ? "#f3f3f3" : "lightblue";
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const playAllButton = document.getElementById("play-all");
+    const changeBgButton = document.getElementById("change-bg");
 
-
-document.getElementById("play-all").addEventListener("click", () => {
-    const audios = document.querySelectorAll("audio");
-    audios.forEach(audio => {
-        audio.play();
+    playAllButton.addEventListener("click", () => {
+        const audios = document.querySelectorAll("audio");
+        audios.forEach(audio => {
+            audio.play();
+        });
     });
+
+    changeBgButton.addEventListener("click", () => {
+        document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    });
+
 });
